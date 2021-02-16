4c89e6671d86499c58a520f99f156827a7047c2a
Gonçalo Figueiredo <Gonalo Figueiredo@DESKTOP-A2K0GVO>
2021-02-16 13:51:00 +0000

    adicionamos db e alteramos clientes para login

---
+++ db.py
@@ -0,0 +1,6 @@
+class DataBase:
+    def __init__(self):
+        self.Host = 'ec2-54-155-208-5.eu-west-1.compute.amazonaws.com'
+        self.Database = 'd9rim2o5kabrk1'
+        self.User = 'khgewnujnbxeha'
+        self.Password = '83aa1afb9a11ab03d14e4b81a3f3b6741aba17e24a12f36eb5db04406ea2dbe6'
